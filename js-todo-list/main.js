0e7e08d6ad8f7359f65333da9fab73c2435d0376
prodigy7748 <prodigy7748g@gmail.com>
2020-12-08 11:45:49 +0800

    toggle

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,11 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('ul li').addEventListener('click',function(){
+    const li = document.querySelector('ul li')
+      li.classList.toggle('checked')
+  })
+
+  document.querySelector('span').addEventListener('click', function(){
+    document.querySelector('span').remove()
+  })
+})
