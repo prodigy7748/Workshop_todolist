f72da1861d85ca73bc3ba14a5e5a778a666f7cca
prodigy7748 <prodigy7748g@gmail.com>
2020-12-08 14:04:09 +0800

    update

--- 04_bonus-drag/main.js
+++ 04_bonus-drag/main.js
@@ -15,20 +15,20 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log('dragend')
   })
 
-  // //target
-  // target.addEventListener('dragenter', function(){
-  //   console.log('dragenter target')
-  // })
+  //target
+  target.addEventListener('dragenter', function(){
+    console.log('dragenter target')
+  })
 
-  // target.addEventListener('dragover', function(){
-  //   console.log('dragover target')
-  // })
+  target.addEventListener('dragover', function(){
+    console.log('dragover target')
+  })
 
-  // target.addEventListener('dragleave', function(){
-  //   console.log('dragleave target')
-  // })
+  target.addEventListener('dragleave', function(){
+    console.log('dragleave target')
+  })
 
-  // target.addEventListener('drop', function(){
-  //   console.log('drop')
-  // })
+  target.addEventListener('drop', function(){
+    console.log('drop')
+  })
 })
