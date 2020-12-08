33e3d3b9689dc56168c06c553c32067fe460a7a9
prodigy7748 <prodigy7748g@gmail.com>
2020-12-08 12:49:40 +0800

    remove li

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,10 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function(){
+  const remove_btn = document.querySelectorAll('.close')
+  const lists = document.querySelectorAll('ul li')
+    for (var i = 0; i < lists.length; i++) {
+      remove_btn[i].addEventListener('click', function(){
+      lists[i].remove()
+    })
+  }
+})
