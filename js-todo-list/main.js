80845e32c0238daa0dc928430944fcef00ba165d
prodigy7748 <prodigy7748g@gmail.com>
2020-12-08 13:53:30 +0800

    done add li

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,17 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function(){
+  const ul = document.querySelector('ul')
+  const addBtn = document.querySelector('#addBtn')
+  
+  addBtn.addEventListener('click',function(){
+    const input_value = document.querySelector('#input').value
+    const li = document.createElement('li')
+    const span = document.createElement('span')
+    span.textContent = ('x')
+    span.classList.add('close')
+    li.textContent = (input_value)
+    console.log(input_value)
+    li.appendChild(span)
+    ul.insertAdjacentElement('beforeEnd', li)
+  })
+})
